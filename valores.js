2b370249510fbfd9d59409e15be1d1d5402b4250
Sr.Lechuga <jonattanlima@gmail.com>
2022-01-10 18:22:10 -0300

    Entendiendo tipos de objetos en javascript

---
+++ valores.js
@@ -0,0 +1,13 @@
+40 //valor tipo numero
+
+"Jonattan Lima" //String
+
+true //Booleano
+
+null
+undefined //Valores placeholder, nulos para los amigos
+
+//Valores primitivos----------------------------
+
+[1,2,3] //array, objeto
+{ nombre: "Diego" } //set, otro objeto
