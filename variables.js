b671bf3612f63842578719df14b23a81d4d76a66
Sr.Lechuga <jonattanlima@gmail.com>
2022-01-10 18:35:40 -0300

    Trabajando con variables

---
+++ variables.js
@@ -0,0 +1,22 @@
+var nombre = "Jonattan"; //se crea variable nombre, con valor Jonattan
+
+//Declarar
+var edad;
+//Inicializar
+edad = 27;
+
+edad //El Tooltiptext aporta info de la variable
+
+//Declaracion e inicializacion
+var elementos = ["Computadora", "Celular"];
+elementos //Trae el objtevo entero
+elementos[1] //trae celular, empieza en 0
+
+var persona = {
+    nombre: "Jonattan",
+    apellido: "Lima",
+    casado = false,
+    edad: 27
+}
+
+persona
