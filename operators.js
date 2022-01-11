32a3cc70dd9e94768dff79d86cee63504aed90b8
Sr.Lechuga <jonattanlima@gmail.com>
2022-01-11 15:51:37 -0300

    Working with operators (seems like we work a lot...)

---
+++ operators.js
@@ -0,0 +1,23 @@
+//Operadores binarios
+3 + 2;
+50 - 10;
+10 * 20;
+20 / 2;
+/*Realizan una operacion matematica entre dos valores */
+
+"Sr." + "Lechuga"; // Concatenacion
+
+var a = 1;  // Asignacion
+3 == "3";   //(true) Comparacion de valores
+3 === "3";  //(false) Comparacion de tipos y valores
+5 < 3;      //(false) "Menor que"
+5 > 3;      //(true)  "Mayor que"
+5 <= 6;     //(true)  "Menor o igual que"
+5 >= 6;     //(false) "Mayor o igual que"
+
+a && b;  // And
+a || b;  // Or 
+!false;  //(true) El signo ! es la negacion, Operador unitario
+
+contador++; //Incremento en 1
+contador += 2; //Incremento la cantidad que quiera
