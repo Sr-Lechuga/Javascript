ebd5cc082e5cffe94f8f1fa5950c4c53c8a61221
Sr.Lechuga <jonattanlima@gmail.com>
2022-01-11 16:54:24 -0300

    Switch clausule

---
+++ switch.js
@@ -0,0 +1,18 @@
+var dish = 1;
+switch (dish) {
+    case 1:
+        console.log("Ramen");        
+        break;
+    
+    case 2:
+        console.log("Sushi");        
+        break;
+
+    case 3:
+        console.log("Crème brûlée");        
+        break;        
+
+    default:
+        console.log("Coffe");
+        break;
+}
