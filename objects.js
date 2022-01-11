b244eb870cb1a380482e49060d3ca767c702d17f
Sr.Lechuga <jonattanlima@gmail.com>
2022-01-11 19:32:02 -0300

    Creating objects using constructors

--- objects.js
+++ objects.js
@@ -6,3 +6,19 @@ var myCar = {
         console.log(`Car ${this.model} ${this.year}`);
     }
 };
+
+myCar.carDetail(); //This is the way to call, like a normal function
+//Not as a propperty...
+
+//Constructor function
+function car(brand, model, year) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+}
+
+var newCar = new car("Tesla","Model 3", 2021);
+//This is how to create a new car
+
+var newCar_2 = new car("Tesla","Model X", 2018);
+var newCar_3 = new car("Toyota","Corola", 2020);
