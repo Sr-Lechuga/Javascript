2d62cf859c9ccef1e36a9f1738fcaab98ee45d8a
Sr.Lechuga <jonattanlima@gmail.com>
2022-01-11 16:13:35 -0300

    If variants

---
+++ if.js
@@ -0,0 +1,22 @@
+// If-Else
+if (true) {
+    console.log("You're right honey!");
+} else {
+    console.log("Anyways, you're right honey!");
+}
+
+// If-ElseIf-Else
+var edad = 18; 
+if (edad === 18) {
+    console.log("You can vote, it will be your fisrt time.")
+} else if (edad > 21) {
+    console.log("Welcome back! You can vote again.")
+} else {
+    console.log("You're too young to vote. Come back next time");
+}
+
+//One line If-Else
+//Sintaxis: condition ? true : false;
+
+var numero = 1;
+numero === 1 ? "Yes, I'm The ONE" : "No, I'm not The ONE";
